68b29fb873bc17c4c9785d8a5514c06aa1de1a04
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-03-30 20:38:21 +0530

    protect route fixed

--- frontend/src/components/protectRoute/TeacherProtect.jsx
+++ frontend/src/components/protectRoute/TeacherProtect.jsx
@@ -5,7 +5,7 @@ const TeacherProtectedRoute = ({ children }) => {
   const username = sessionStorage.getItem("username");
 
   
-  if (!username || !username.startsWith("tech#")) {
+  if (!username || !username.startsWith("teacher")) {
 
     alert("You are not authenticated. Please log in as a teacher.");
     return <Navigate to="/" replace  />;
