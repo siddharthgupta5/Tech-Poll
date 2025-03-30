e6a7d0ef8bfc1bd91294b72fb80b2adfbc893171
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-03-30 19:37:44 +0530

    backend completed & frontend initailzed

---
+++ frontend/src/components/protectRoute/TeacherProtect.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const TeacherProtectedRoute = ({ children }) => {
+  const username = sessionStorage.getItem("username");
+
+  
+  if (!username || !username.startsWith("tech#")) {
+
+    alert("You are not authenticated. Please log in as a teacher.");
+    return <Navigate to="/" replace  />;
+  }
+
+  return children;
+};
+
+export default TeacherProtectedRoute;
